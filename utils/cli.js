5856ddb33898a0782c10f01205f8ea6ef3d4fbc5
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-11-22 13:05:21 -0500

    Move Default Parameters Outside of Export for Search Function

--- utils/cli.js
+++ utils/cli.js
@@ -2,14 +2,13 @@ const axios = require('axios');
 
 // base url
 const baseUrl = 'https://api.stackexchange.com/2.2/search/advanced';
+// default params
+const order = 'desc';
+const sort = 'relevance';
+const site = 'stackoverflow';
+const filter = '!)rTkraPXy17fPqpx7wE5';
 
 module.exports = async question => {
-	// default params
-	const order = 'desc';
-	const sort = 'relevance';
-	const site = 'stackoverflow';
-	const filter = '!)rTkraPXy17fPqpx7wE5';
-
 	try {
 		const { data } = await axios.get(
 			`${baseUrl}?order=${order}&sort=${sort}&q=${question}&site=${site}&filter=${filter}`
