5a3e84508ddb7c61edc42827ac222d59ee8a72b9
msaaddev <mrsaadirfan@gmail.com>
2020-11-23 21:55:05 +0500

    👌 IMPROVE: spinner on API call

--- utils/cli.js
+++ utils/cli.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const ora = require('ora');
 
 // base url
 const baseUrl = 'https://api.stackexchange.com/2.2/search/advanced';
@@ -9,13 +10,21 @@ const site = 'stackoverflow';
 const filter = '!)rTkraPXy17fPqpx7wE5';
 
 module.exports = async question => {
+	// spinner
+	const spinner = ora();
+
+	// making API call
 	try {
+		console.log('');
+		spinner.start();
 		const { data } = await axios.get(
 			`${baseUrl}?order=${order}&sort=${sort}&q=${question}&site=${site}&filter=${filter}`
 		);
 		const { items } = data;
 		console.log(items);
+		spinner.stop();
 	} catch (err) {
+		spinner.stop();
 		console.error(`Error: ${err.response.data.error_message}`);
 	}
 };
