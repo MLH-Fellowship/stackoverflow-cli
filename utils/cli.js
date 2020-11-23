99d88ef0f6f9722298296045821b3472ef041e3d
angelinag <andji.gash@gmail.com>
2020-11-23 20:04:51 +0200

    Added character decoding

--- utils/cli.js
+++ utils/cli.js
@@ -8,12 +8,35 @@ const sort = 'relevance';
 const site = 'stackoverflow';
 const filter = '!)rTkraPXy17fPqpx7wE5';
 
+function decodeEntities(encodedString) {
+    var translate_re = /&(nbsp|amp|quot|lt|gt);/g;
+    var translate = {
+        "nbsp":" ",
+        "amp" : "&",
+        "quot": "\"",
+        "lt"  : "<",
+        "gt"  : ">"
+    };
+    return encodedString.replace(translate_re, function(match, entity) {
+        return translate[entity];
+    }).replace(/&#(\d+);/gi, function(match, numStr) {
+        var num = parseInt(numStr, 10);
+        return String.fromCharCode(num);
+    });
+}
+
 module.exports = async question => {
 	try {
-		const { data } = await axios.get(
+		let { data } = await axios.get(
 			`${baseUrl}?order=${order}&sort=${sort}&q=${question}&site=${site}&filter=${filter}`
 		);
-		const { items } = data;
+		// decode html characters to regular chars
+		for (const [key, value] of Object.entries(data['items'])) {
+			item = value["body_markdown"];
+			data['items'][key]['body_markdown'] = decodeEntities(item);
+		}
+		
+		let { items } = data;
 		console.log(items);
 	} catch (err) {
 		console.error(`Error: ${err.response.data.error_message}`);
