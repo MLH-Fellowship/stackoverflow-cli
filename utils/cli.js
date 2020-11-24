1a28b61b4b4ea8420e91cbdd407797537e513520
msaaddev <mrsaadirfan@gmail.com>
2020-11-24 16:50:53 +0500

    👌 IMPROVE: code and spinner

--- utils/cli.js
+++ utils/cli.js
@@ -6,6 +6,7 @@ const baseUrl = 'https://api.stackexchange.com/2.2/search/advanced';
 // default params
 const site = 'stackoverflow';
 const filter = '!)rTkraPXy17fPqpx7wE5';
+const pageSize = 10;
 
 const decodeEntities = encodedString => {
 	const translate_re = /&(nbsp|amp|quot|lt|gt);/g;
@@ -17,10 +18,10 @@ const decodeEntities = encodedString => {
 		gt: '>'
 	};
 	return encodedString
-		.replace(translate_re, function (match, entity) {
+		.replace(translate_re, (match, entity) => {
 			return translate[entity];
 		})
-		.replace(/&#(\d+);/gi, function (match, numStr) {
+		.replace(/&#(\d+);/gi, (match, numStr) => {
 			const num = parseInt(numStr, 10);
 			return String.fromCharCode(num);
 		});
@@ -28,7 +29,7 @@ const decodeEntities = encodedString => {
 
 module.exports = async (question, flags) => {
 	// spinner
-	const spinner = ora();
+	const spinner = ora(`Fetching results for your query...`);
 
 	// default params
 	const order = flags.indexOf(`--asc`) >= 0 ? 'asc' : 'desc';
@@ -48,9 +49,9 @@ module.exports = async (question, flags) => {
 		console.log('');
 		spinner.start();
 		const { data } = await axios.get(
-			`${baseUrl}?order=${order}&sort=${sort}&q=${question}&site=${site}&filter=${filter}`
+			`${baseUrl}?order=${order}&sort=${sort}&q=${question}&pageSize=${pageSize}&site=${site}&filter=${filter}`
 		);
-
+		spinner.succeed();
 		// decode html characters to regular chars
 		for (const [key, value] of Object.entries(data['items'])) {
 			let item = value['body_markdown'];
@@ -62,19 +63,18 @@ module.exports = async (question, flags) => {
 			data['items'][key]['body'] = [];
 
 			// Uncomment the code below in order to concat the body_markdown array into one string
-			//let whole_string = '';
-			//for(substring_key in data['items'][key]['body_markdown']){
-			//	whole_string += data['items'][key]['body_markdown'][substring_key];
-			//}
-			//data['items'][key]['body_markdown'] = whole_string
+			// let whole_string = '';
+			// for (substring_key in data['items'][key]['body_markdown']) {
+			// 	whole_string +=
+			// 		data['items'][key]['body_markdown'][substring_key];
+			// }
+			// data['items'][key]['body_markdown'] = whole_string;
 		}
 
 		let { items } = data;
-		console.log(items);
-
-		spinner.stop();
+		console.log('\n\n', items[0]);
 	} catch (err) {
-		spinner.stop();
+		spinner.fail();
 		console.error(`Error: ${err.response.data.error_message}`);
 	}
 };
