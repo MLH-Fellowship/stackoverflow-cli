f7a4e34a3347eb7f5c77968acae7118510476954
yobinmok <dyqls1214@naver.com>
2022-05-18 16:12:02 +0900

    selected Q&A

--- utils/switch.js
+++ utils/switch.js
@@ -1,20 +1,20 @@
 const keypress = require('keypress');
 const logUpdate = require('log-update'); // Log by overwriting the previous output in the terminal.
 const chalk = require('chalk');
+const boxen = require('boxen');
 const { info } = require('log-symbols'); // Colored symbols for various log levels
 const end = require('./end');
 const output = require('./output');
-const index = require('../new');
+let check=""
 
 /**
  *
  * @param index - thread number
  * @return string - formated string
  */
-// TITLE, QUESTION, ANSWER #n이 아예 안뜰때도 있음 -> 터미널 화면보다 출력되는 값이 더 크면 잘리는 상황 -> console.log는 괜찮고 logUpdate만 그럼
 
 const formatThread = (indexOfThread, indexOfAns, thread, order, sort) => {
-	return `${info} ${chalk.dim( // dim - Make the text have lower opacity
+	return `${check}${info} ${chalk.dim( // dim - Make the text have lower opacity
 		`Thread #${indexOfThread + 1} | Order: ${order} | Sort By: ${sort}`
 	)}\n\n${chalk.hex(`#14b514`).bold.inverse(`   TITLE    `)} ${ // inverse- Invert background and foreground colors.
 		thread[indexOfThread].title
@@ -38,43 +38,72 @@ const formatThread = (indexOfThread, indexOfAns, thread, order, sort) => {
 module.exports = (threads, order, sort) => {
 	let counterOfThread = 0;
 	let counterOfAnswer = 0;
-	logUpdate(formatThread(counterOfThread, 0, threads, order, sort))
+	let saveThread;
+	let saveAnswer;
+
 	console.log(formatThread(counterOfThread, 0, threads, order, sort))
 
 	// switch the result back and forth from left and right arrow keys and exits with escape key
 	keypress(process.stdin);
 	process.stdin.on('keypress', function (ch, key) {
 		if (key.name === 'right' && counterOfThread !== threads.length - 1) { // Thread가 끝나지 않았다면 ~하는 조건문
+			console.clear()
 			counterOfThread++;
 			counterOfAnswer = 0;
-			//logUpdate(formatThread(counterOfThread, 0, threads, order, sort))
-			console.log(formatThread(counterOfThread, 0, threads, order, sort))
+			if(saveThread == counterOfThread && saveAnswer == 0){
+				check = "< Selected Question & Answer >\n\n"
+				console.log(boxen(formatThread(counterOfThread, 0, threads, order, sort)))
+				check=""
+			}else
+				console.log(formatThread(counterOfThread, 0, threads, order, sort))
 		}
 		if (key.name === 'left' && counterOfThread !== 0) {
+			console.clear()
 			counterOfThread--;
 			counterOfAnswer = 0;
-			//logUpdate(formatThread(counterOfThread, 0, threads, order, sort))
-			console.log(formatThread(counterOfThread, 0, threads, order, sort))
+			if(saveThread == counterOfThread && saveAnswer == 0){
+				check = "< Selected Question & Answer >\n\n"
+				console.log(boxen(formatThread(counterOfThread, 0, threads, order, sort)))
+				check=""
+			}else
+				console.log(formatThread(counterOfThread, 0, threads, order, sort))
 		}
 		if (
 			key.name === 'up' &&
 			counterOfAnswer !== threads[counterOfThread].answers.length - 1
 		) {
+			console.clear()
 			counterOfAnswer++;
-			//logUpdate(formatThread(counterOfThread, counterOfAnswer, threads, order, sort))
-			console.log(formatThread(counterOfThread, counterOfAnswer, threads, order, sort))
+			if(saveThread == counterOfThread && saveAnswer == counterOfAnswer){
+				check = "< Selected Question & Answer >\n\n"
+				console.log(boxen(formatThread(counterOfThread, counterOfAnswer, threads, order, sort)))
+				check=""
+			}else
+				console.log(formatThread(counterOfThread, counterOfAnswer, threads, order, sort))
 		}
 		if (key.name === 'down' && counterOfAnswer !== 0) {
+			console.clear()
 			counterOfAnswer--;
-			//logUpdate(formatThread(counterOfThread, counterOfAnswer, threads, order, sort))
-			console.log(formatThread(counterOfThread, counterOfAnswer, threads, order, sort))
+			if(saveThread == counterOfThread && saveAnswer == counterOfAnswer){
+				check = "< Selected Question & Answer >\n\n"
+				console.log(boxen(formatThread(counterOfThread, counterOfAnswer, threads, order, sort)))
+				check=""
+			}else
+				console.log(formatThread(counterOfThread, counterOfAnswer, threads, order, sort))
 		}
 		if (key.name === 'escape') {
 			end();
 			process.exit();
 		}
-		if (key.name === 'r'){
-			index();
+		if (key.name === 's'){
+			check="< Selected Question & Answer >\n\n"
+			saveThread = counterOfThread;
+			saveAnswer = counterOfAnswer;
+			console.log(boxen(formatThread(saveThread, saveAnswer, threads, order, sort)))
+			check=""
+		}
+		if(key.name === 'c'){
+			console.clear()
 		}
 	});
 	process.stdin.setRawMode(true);
